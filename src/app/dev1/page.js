73b42e92746c902de4018d4ae08e1299c3018f43
Faruks23 <mdfaruksp2@gmail.com>
2023-08-07 14:30:18 +0600

    add dev1 page by de21

--- src/app/dev1/page.js
+++ src/app/dev1/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DevPage = () => {
   return (
     <div>
-      <h1>This is Dev pag added by dev 1 </h1>
+      <h1>This is Dev pag added by dev 2 </h1>
       
      <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum magnam libero officiis nemo fugiat tempora voluptates? Illo architecto, dignissimos exercitationem deleniti ex ducimus dolorem. Adipisci reiciendis consequatur officia itaque laborum!</p>
     </div>
