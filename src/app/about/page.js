5dace1643010d4598f4e4ea142381fb2ff7bf640
Faruks23 <mdfaruksp2@gmail.com>
2023-08-08 00:01:47 +0600

    Commit message

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,6 +8,14 @@ const AboutPage = () => {
   return (
     <div>
       <h1 className='text-2xl'>This is AboutPage</h1>
+      <div className="about">
+        <h1>name:Md Faruk</h1>
+        <h1>email:mdfaruksp2@gmail.com</h1>
+        <h1>phone:01882759955</h1>
+       
+        
+        
+      </div>
     </div>
   );
 };
